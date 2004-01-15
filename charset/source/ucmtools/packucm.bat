0586cb88cc0a7c5c41c81b331f5c82d759d7f7ae
George Rhoten <grhoten@users.noreply.github.com>
2004-01-15 01:29:56 +0000

    [ICU-3506] Update this batch file to use the modern pkgdata syntax
    X-SVN-Rev: 14327

--- charset/source/ucmtools/packucm.bat
+++ charset/source/ucmtools/packucm.bat
@@ -8,6 +8,6 @@ set ICUDATA_PACKAGE=%1
 if "%1" == "" set ICUDATA_PACKAGE=icudata
 @echo Creating a package for %ICUDATA_PACKAGE%
 for %%i in (%1*.ucm) do makeconv -p %ICUDATA_PACKAGE% %%i && echo %ICUDATA_PACKAGE%_%%~ni.cnv >> %TEMP%\templist.txt
-pkgdata -p %ICUDATA_PACKAGE% -m common -O R:C:\Development\icu\ %TEMP%\templist.txt
+pkgdata -p %ICUDATA_PACKAGE% -m common %TEMP%\templist.txt
 endlocal
 del %TEMP%\templist.txt
