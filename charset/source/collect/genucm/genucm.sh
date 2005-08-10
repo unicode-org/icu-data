166c9ec4460200e05275273f06d52ac52f12be05
George Rhoten <grhoten@users.noreply.github.com>
2005-08-10 19:56:55 +0000

    [ICU-3449] Make it easier to build the tool that collects mapping tables.
    X-SVN-Rev: 18420

---
+++ charset/source/collect/genucm/genucm.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright (C) 2005, International Business Machines
+#   Corporation and others.  All Rights Reserved.
+
+OS_INFO="`uname -a`"
+OS_VERSION="`uname -r`"
+OS_TYPE="`uname`"
+ICU_ARGS="-I../../../../icu/source/common -L../../../../icu/source/lib -licuuc -licudata"
+ICU_LIB_PATH="../../../../icu/source/lib"
+
+echo "Collecting Unicode conversion mappings for $OS_TYPE"
+case "$OS_TYPE" in
+    AIX)
+        FILES="genucm.cpp iconv.cpp"
+        xlC_r $ICU_ARGS $FILES -liconv -o genucm
+        LIBPATH=$ICU_LIB_PATH genucm $*
+        ;;
+    SunOS)
+        FILES="genucm.cpp iconv.cpp"
+        g++ $ICU_ARGS $FILES -o genucm
+        LD_LIBRARY_PATH=$ICU_LIB_PATH genucm $*
+        ;;
+    HP-UX)
+        FILES="genucm.cpp iconv.cpp"
+        aCC $ICU_ARGS -licuuc -licudata -ldld $FILES -o genucm
+        SHLIB_PATH=$ICU_LIB_PATH genucm $*
+        ;;
+    Linux)
+        FILES="genucm.cpp iconv.cpp"
+        g++ $ICU_ARGS $FILES -o genucm
+        LD_LIBRARY_PATH=$ICU_LIB_PATH genucm $*
+        ;;
+    *)
+        echo "Don't know how to get the list of mappings for $OS_TYPE"
+        ;;
+esac
+
