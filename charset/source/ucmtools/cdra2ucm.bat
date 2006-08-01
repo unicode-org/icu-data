7aaa3f84bc1db97a6db5015ebafd25df65a641c4
George Rhoten <grhoten@users.noreply.github.com>
2006-08-01 06:31:20 +0000

    [ICU-5234] Make some private tools public for iterating generating all UCM files.
    X-SVN-Rev: 19941

---
+++ charset/source/ucmtools/cdra2ucm.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Copyright Copyright (C) 2000-2006, International Business Machines Corporation and others.
+rem All Rights Reserved.
+rem echo Remember to put the rptp2ucm program in the PATH
+for %%i in (*.r?map*) do echo Converting %%~fi && rptp2ucm %%i
