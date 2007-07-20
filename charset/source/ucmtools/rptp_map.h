e5ed6b2de8da7511a963e4fe2b5d343cb11f630a
George Rhoten <grhoten@users.noreply.github.com>
2007-07-20 18:53:11 +0000

    [ICU-5234] Add review comments.
    X-SVN-Rev: 22078

--- charset/source/ucmtools/rptp_map.h
+++ charset/source/ucmtools/rptp_map.h
@@ -13,13 +13,26 @@ struct FilenameMapping;
 
 /*
 This class allows the rptp2ucm tool to remember what combination of files generated which UCM file.
+The history file is read via the constructor, updated via getFilename(), and
+intended to then be written via writeHistoryFile()
 */
 class FilenameMappingHistory {
 public:
     static FilenameMappingHistory *create(const char *filename, UErrorCode *status);
 
+    /*
+    Based on rpmap filename, tpmap filename and year, generate an appropriate
+    UTS#22 filename. The results are stored in rptp-history.txt. If that file
+    contains a pre-existing filename, that will be returned. If another rpmap
+    filename and tpmap filename combination generates a conflicting UTS#22 filename,
+    then a longer (newer) more unique filename will be returned instead.
+    */
     const char *getFilename(const char *rpmapFilename, const char *tpmapFilename, uint16_t year, UErrorCode *status);
 
+    /*
+    Save the generated names to a file so that we can generate the same UTS#22
+    filename when this tool is run again at a future date.
+    */
     void writeHistoryFile(UErrorCode *status);
 
     ~FilenameMappingHistory();
